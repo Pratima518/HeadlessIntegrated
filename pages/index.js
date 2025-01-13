038b62557b6cf52759b166b6c9878fd6bfb4d517
Pratima Edarapalli <pratima.edarapalli@pega.com>
2025-01-13 12:55:29 +0530

    added App.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-//import Link from 'next/link';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
+//import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 
 // Import Amplify and configuration
-//import { Amplify } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 //import awsExports from './aws-exports'; // Import your AWS Amplify configuration
 import { AuthProvider } from "react-oidc-context";
 
@@ -19,41 +18,12 @@ const cognitoAuthConfig = {
   scope: "email openid phone",
 };
 
-function Home() {
-  const auth = useAuth();
-  const router = useRouter(); // Initialize Next.js router for redirection
-
-  const signOutRedirect = () => {
-    const clientId = "6v3vrfja0ofkuckrbde5fe0t6h";
-    const logoutUri = "https://master.d3nx2e36z49zwg.amplifyapp.com";
-    const cognitoDomain = "https://us-east-1x5evneaas.auth.us-east-1.amazoncognito.com";
-    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
-  };
-  // Automatically redirect to /create-provider if the user is authenticated
-  useEffect(() => {
-    if (auth.isAuthenticated) {
-      router.push("/create-provider"); // Redirect to CreateProvider page
-    }
-  }, [auth.isAuthenticated, router]);
-
-  if (auth.isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (auth.error) {
-    return <div>Error: {auth.error.message}</div>;
-  }
-
-  if (auth.isAuthenticated) {
-    return <div>Redirecting to Create Provider...</div>;
-  }
-
-  return (
-    <div>
-      <h1>Welcome to LPHeadlessIntegrated</h1>
-      <button onClick={() => auth.signinRedirect()}>Sign In</button>
-    </div>
-  );
-}
-
-export default Home;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// wrap the application with AuthProvider
+root.render(
+  <React.StrictMode>
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
