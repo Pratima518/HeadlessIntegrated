341c2339ada5b6c3f4aa9b740b914b184932b696
Pratima Edarapalli <pratima.edarapalli@pega.com>
2025-01-13 16:06:50 +0530

    added AuthProvider to render on client side

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 import App from './App';
 import { AuthProvider } from 'react-oidc-context';
 //import awsExports from '../aws-exports'; // Adjust the path as needed
 //import '../styles/globals.css'; // Import your global styles
+
+const AuthProvider = dynamic(
+  () => import('react-oidc-context').then(mod => mod.AuthProvider),
+  { ssr: false }
+);
 
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_X5evnEAAS",
